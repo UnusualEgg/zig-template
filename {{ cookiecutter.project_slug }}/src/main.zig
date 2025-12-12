8cbb5649750a1c04ec4765a21475cba6eb7e6bf8
UnusualEgg <shotgun282006@gmail.com>
2025-12-12 13:31:36 -0600

    fix stdout writer

--- {{ cookiecutter.project_slug }}/src/main.zig
+++ {{ cookiecutter.project_slug }}/src/main.zig
@@ -1,9 +1,7 @@
 const std = @import("std");
 
 pub fn main() !void {
-    var stdout_buffer: [2048]u8 = undefined;
-    const stdout = std.fs.File.stdout().writer(&stdout_buffer);
-    const w = stdout.interface;
+    var stdout = std.fs.File.stdout().writer(&.{);
+    const w = &stdout.interface;
     try w.print("Hello world", .{});
-    try w.flush();
 }
