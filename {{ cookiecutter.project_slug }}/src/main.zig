bcb50380add3d6a14674ee0d9fa2e06d91a9c4de
UnusualEgg <shotgun282006@gmail.com>
2026-02-26 13:55:11 -0600

    add newline to hello world message

--- {{ cookiecutter.project_slug }}/src/main.zig
+++ {{ cookiecutter.project_slug }}/src/main.zig
@@ -3,5 +3,5 @@ const std = @import("std");
 pub fn main() !void {
     var stdout = std.fs.File.stdout().writer(&.{});
     const w = &stdout.interface;
-    try w.print("Hello world", .{});
+    try w.print("Hello world\n", .{});
 }
